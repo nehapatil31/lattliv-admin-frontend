68a1c830ece3182e6dff21d5c167aad009664b89
Neha <neha@wishup.co>
2022-06-11 20:50:14 +0530

    dashboard product cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,7 +9,56 @@ function Home() {
             <Sidebar />
             <div className="homeContainer">
                 <Navbar />
-                Analytics cards
+                <h2>Products</h2>
+                <div class="row">
+                    <div class="card">
+                        <div class="card-top">
+                        </div>
+                        <div class="card-body">
+                            <div className="number-container">
+                            <div className="number">25 </div>
+                            <div className="number-text">Published</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-top">
+                        </div>
+                        <div class="card-body">
+                            <div className="number-container" style={{color:"rgb(255, 193, 7)"}}>
+                            <div className="number">121 </div>
+                            <div className="number-text">Saved</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-top">
+                        </div>
+                        <div class="card-body">
+                            <div className="number-container" style={{color:"rgb(244, 67, 54)"}}>
+                            <div className="number">97 </div>
+                            <div className="number-text">Hidder</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div class="card">
+                        <div class="card-top">
+                        </div>
+                        <div class="card-body">
+                            <p id="sub">Maybe a sub-heading</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus quam in libero lacinia interdum. Vivamus sed sem vel dui suscipit volutpat. Maecenas ultricies nibh lacus,</p>
+                        </div>
+                        <div class="card-footer">
+                            <ul class="card-actions">
+                                <a href=""> <li>Share</li></a>
+                                <a href=""> <li>Read more</li></a>
+                                <a href=""> <li>Contact</li></a>
+                            </ul>
+                        </div>
+                    </div> */}
+                </div>
+               
+
             </div>
         </div>
         );
