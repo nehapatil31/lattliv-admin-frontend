019665bd81d94a3abd380d813574b9221fa444b8
Neha <neha@wishup.co>
2022-07-24 15:11:49 +0530

    Dashboard ui changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -39,25 +39,25 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-    {name:'NifluxLed Light Bulb', count:159},
-    {name:'The Slim Wallet for Men', count:237},
-    {name:'Smart Clock', count:262},
-    {name:'Home Decorative Metal Cycle', count:305},
-    {name:'Artificial Plant with Pot ', count:356}
+    { name: 'NifluxLed Light Bulb', count: 159 },
+    { name: 'The Slim Wallet for Men', count: 237 },
+    { name: 'Smart Clock', count: 262 },
+    { name: 'Home Decorative Metal Cycle', count: 305 },
+    { name: 'Artificial Plant with Pot ', count: 356 }
 ];
-const rows_category =[
-    {name:'Books', count:159},
-    {name:'Fashion', count:237},
-    {name:'Electronics', count:262},
-    {name:'Baby', count:305},
-    {name:'Gift cards ', count:356}
+const rows_category = [
+    { name: 'Books', count: 159 },
+    { name: 'Fashion', count: 237 },
+    { name: 'Electronics', count: 262 },
+    { name: 'Baby', count: 305 },
+    { name: 'Gift cards ', count: 356 }
 ]
-const rows_blog =[
-    {name:'Affordable shopping for all', count:159},
-    {name:'#LattlivWrapped2021', count:237},
-    {name:'Everything you need to know about fast fashion', count:262},
-    {name:'Fast Home Decoration Types', count:305},
-    {name:'', count:''}
+const rows_blog = [
+    { name: 'Affordable shopping for all', count: 159 },
+    { name: '#LattlivWrapped2021', count: 237 },
+    { name: 'Everything you need to know about fast fashion', count: 262 },
+    { name: 'Fast Home Decoration Types', count: 305 },
+    { name: '', count: '' }
 ]
 
 export const options = {
@@ -96,14 +96,14 @@ export const data = {
 };
 
 function Home() {
-    let getData = function(type){
-        if(type=='product'){
+    let getData = function (type) {
+        if (type == 'product') {
             return rows;
-        }else if(type=='category'){
+        } else if (type == 'category') {
             return rows_category
-        }else if(type=='blog'){
+        } else if (type == 'blog') {
             return rows_blog;
-        }else if(type=='visitor'){
+        } else if (type == 'visitor') {
             return rows
         }
     }
@@ -130,68 +130,62 @@ function Home() {
 
                         <Grid item xs={8}>
                             <div className="bar-container">
-                            <Bar options={options} data={data} />
+                                <Bar options={options} data={data} />
                             </div>
                         </Grid>
                         <Grid item xs={4}>
-                            <Grid container spacing={2}>
+                            <div className="views-table">
+                                <Grid container spacing={2}>
 
-                                {/* <Grid item xs={4}>
-                                </Grid> */}
-                                <Grid item xs={8} style={{ textAlign: 'left' }}>
-                                    <h3>Views</h3>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} style={{ float: 'right' }}>
-                                        <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
-                                        <Select
-                                            labelId="demo-simple-select-standard-label"
-                                            id="demo-simple-select-standard"
-                                            value={type}
-                                            onChange={handleChange}
-                                            label="Type"
-                                        >
-                                            <MenuItem value='product'>Products</MenuItem>
-                                            <MenuItem value='category'>Categories</MenuItem>
-                                            <MenuItem value='blog'>Blogs</MenuItem>
-                                            <MenuItem value='visitor'>Visitors</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                            </Grid>
-
-
-
-                            <br />
-                            <TableContainer component={Paper}>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell style={{fontWeight:"600"}}>Title</TableCell>
-                                            <TableCell style={{fontWeight:"600"}} align="right">Count</TableCell>
-                                            {/* <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {tableData.map((row) => (
-                                            <TableRow
-                                                key={row.name}
-                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    <Grid item xs={8} style={{ textAlign: 'left' }}>
+                                        <h3 style={{paddingLeft: "12px"}}>Views</h3>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} style={{ float: 'right' }}>
+                                            <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-standard-label"
+                                                id="demo-simple-select-standard"
+                                                value={type}
+                                                onChange={handleChange}
+                                                label="Type"
                                             >
-                                                <TableCell component="th" scope="row">
-                                                    {row.name}
-                                                </TableCell>
-                                                <TableCell align="right">{row.count}</TableCell>
-                                                {/* <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell> */}
+                                                <MenuItem value='product'>Products</MenuItem>
+                                                <MenuItem value='category'>Categories</MenuItem>
+                                                <MenuItem value='blog'>Blogs</MenuItem>
+                                                <MenuItem value='visitor'>Visitors</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+
+
+
+
+                                <TableContainer>
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell style={{ fontWeight: "600", opacity:"0.5" }}>Title</TableCell>
+                                                <TableCell style={{ fontWeight: "600", opacity:"0.5" }} align="right">Count</TableCell>
                                             </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
+                                        </TableHead>
+                                        <TableBody>
+                                            {tableData.map((row) => (
+                                                <TableRow
+                                                    key={row.name}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="right">{row.count}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
                         </Grid>
                     </Grid>
 
